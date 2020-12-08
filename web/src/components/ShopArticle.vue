2528ceaa7f7255195213d27d05f62dabfa3f413e
Anton Laurell <antonlaurell@hotmail.com>
2020-12-08 18:07:50 +0100

    setup(web): get from api

---
+++ web/src/components/ShopArticle.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ title }}</h1>
+    <small>{{ price }}$</small>
+    <p>{{ description }}</p>
+    <p>uploaded <b>{{ created_at }}</b></p>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        description: String,
+        price: Number,
+        created_at: String,
+        id: Number
+    }
+}
+</script>
--- web/src/views/Articles.vue
+++ web/src/views/Articles.vue
@@ -1,14 +1,30 @@
 <template>
-    <p>{{ msg }}</p>
+    <p v-if="articles.length === 0">No articles</p>
+    <shop-article v-for="article in articles" :="article"></shop-article>
 </template>
 
 <script>
+import ShopArticle from '@/components/ShopArticle.vue'
+
 export default {
     name: 'Articles',
+    components: {
+        'shop-article': ShopArticle
+    },
     data() {
         return {
-            msg: 'Here are all articles'
+            articles: []
         }
+    },
+    methods: {
+        loadArticles() {
+            fetch('http://localhost:5000/api/articles/')
+                .then(response => response.json())
+                .then(json => (this.articles = json.articles))
+        }
+    },
+    created() {
+        this.loadArticles()
     }
 }
 </script>
