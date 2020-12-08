15b602ba28caee0838f1e7d0c7a6395812bf3aa1
Anton Laurell <antonlaurell@hotmail.com>
2020-12-08 20:54:13 +0100

    feat(api): flask-script and flask-migrate back in business

---
+++ api/manager.py
@@ -0,0 +1,27 @@
+"""
+manage.py
+- provides a command line utility for interacting with the
+application to perform interactive debugging and setup
+"""
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from shop.app import create_app
+from shop.models import db, Article
+
+app = create_app()
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def shell_ctx():
+    return dict(app=app,
+                db=db,
+                Article=Article)
+
+if __name__ == '__main__':
+    manager.run()
