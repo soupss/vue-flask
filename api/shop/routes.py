c64f3b566effa170e15db965f6cfb285f0cea789
Anton Laurell <antonlaurell@hotmail.com>
2020-12-08 17:21:01 +0100

    fix(api): use make_response

--- api/shop/routes.py
+++ api/shop/routes.py
@@ -3,7 +3,7 @@ routes.py
 - provides the api endpoints, or routes
 """
 
-from flask import Blueprint, jsonify, request, Response, abort
+from flask import Blueprint, jsonify, request, make_response, abort
 from sqlalchemy.exc import IntegrityError
 from .models import db, Article
 
@@ -14,14 +14,14 @@ api = Blueprint('api', __name__)
 def get_articles():
     articles = Article.query.all()
     response = {'articles': [a.to_dict() for a in articles]}
-    return jsonify(response)
+    return make_response(jsonify(response), 200)
 
 # article by id
 @api.route('/articles/<int:id>/', methods=['GET'])
 def get_article_by_id(id):
     article = Article.query.get_or_404(id)
     response = {'article': article.to_dict()}
-    return jsonify(response)
+    return make_response(jsonify(response), 200)
 
 # add new article
 @api.route('/articles/', methods=['POST'])
@@ -31,8 +31,11 @@ def add_article():
     db.session.add(article)
     try:
         db.session.commit()
-        response = Response('Article created successfully', 201, mimetype='application/json')
+        response = make_response('Article created successfully', 201)
     except IntegrityError:
         db.session.rollback()
-        response = Response(f'Unable to create article: An article with title:"{article.title}" already exists.', 409)
+        response = make_response(f'Unable to create article: An article with title:"{article.title}" already exists.', 409)
     return response
+
+#TODO: update_article PUT
+#TODO: delete_article POST
