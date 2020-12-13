b677e72dfc3ee65c48bf436bf493491cef61046e
Anton Laurell <antonlaurell@hotmail.com>
2020-12-13 14:07:34 +0100

    style(nav): started on styling main navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-        <router-link to="/articles">Articles</router-link> |
-        <router-link to="/articles/new">New Article</router-link>
+    <div class="container">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categories
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
+                                <li> <router-link class="dropdown-item" to="/articles">All</router-link> </li>
+                                <li> <router-link class="dropdown-item" to="/articles">Vehicles</router-link> </li>
+                                <li> <router-link class="dropdown-item" to="/articles">Clothes</router-link> </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/articles/new">New Article</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <router-view :key="$route.path"/>
     </div>
-    <router-view :key="$route.path"/>
 </template>
