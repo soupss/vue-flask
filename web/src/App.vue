0cdf6078e44cf8f65c797b7a2868f31f1832eee6
Anton Laurell <antonlaurell@hotmail.com>
2020-12-21 23:57:53 +0100

    style(web/nav): icons and spacing between pills

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,10 +2,16 @@
     <nav class="navbar navbar-dark bg-dark justify-content-center">
         <div class="nav nav-pills">
             <router-link class="text-center nav-link" :class="{ active : this.$route.name === 'All Articles' }" to="/articles">
-                Show Articles
+                <i class="fas fa-stream"></i>
+                <span class="ms-2">
+                    Show Articles
+                </span>
             </router-link>
-            <router-link class="text-center nav-link" :class="{ active : this.$route.name === 'New Article' }" to="/articles/new">
-                New Article
+            <router-link class="ms-2 text-center nav-link" :class="{ active : this.$route.name === 'New Article' }" to="/articles/new">
+                <i class="fas fa-plus"></i>
+                <span class="ms-2">
+                    New Article
+                </span>
             </router-link>
         </div>
     </nav>
